0296a9712186ee400ce536d98cdf338719de4aab
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-10-08 14:37:29 +0200

    Update current feature

--- install.sh
+++ install.sh
@@ -47,6 +47,9 @@ EOF
 kubectl get elasticsearch
 kubectl get pods --selector='elasticsearch.k8s.elastic.co/cluster-name=quickstart'
 
+
+# Wait for secret to appear
+sleep 5
 PASSWORD=$(kubectl get secret quickstart-es-elastic-user -o go-template='{{.data.elastic | base64decode}}')
 
 # https://www.elastic.co/guide/en/cloud-on-k8s/current/k8s-deploy-kibana.html
